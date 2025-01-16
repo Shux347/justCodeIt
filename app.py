e102eafba7acd23ce4fafc2db99b9f8d972ddaed
Lacept <57280174+Lacept@users.noreply.github.com>
2025-01-17 02:12:49 +0800

    hello

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, send_from_directory
 import os
 
-app = Flask(__name__, static_folder='build')
+app = Flask(__name__, static_folder='frontend/build')
 app.config.from_object('config.Config')
 
 # Initialize the database
